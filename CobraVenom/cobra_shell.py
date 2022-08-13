64b192a0914cb7e2b48708b01c8db05d5b778d86
Sidecans <110431851+Sidecans@users.noreply.github.com>
2022-08-12 18:58:49 -0700

    Made cls command

--- CobraVenom/cobra_shell.py
+++ CobraVenom/cobra_shell.py
@@ -6,25 +6,33 @@ with open("destination.txt", 'r') as f:
 class Scanner():
     def __init__(self, chunks):
         self.keyword = chunks[0]
+        self.exception()
         try:    
             self.command = chunks[1]
             self.exe_command()
         except:
             print("Error: Errno [1]: Command Not Provided")
+    
     def exe_command(self):
         if self.keyword == "cd":
             if os.path.exists(login + "\\" + self.command):
                 with open("destination.txt", 'w+') as F:
                     F.write(login + "\\" + self.command)
-            
-
-while True:
+        else:
+            print("Error: Errno[2]: Unkown Command")
+    def exception(self):
+        if self.keyword == "cls":
+            os.system('cls')
+            cobra_shell()
+def cobra_shell():
     with open("destination.txt", 'r') as f:
         login = str(f.read())
     print(str(login))
     text = input("Cobra > ")
     chunks = text.split(' ')
     Scanner(chunks)
+while True:
+    cobra_shell()
 
     
     
