c7f51936ed409204744fe40b6798e77fe6ab3cb1
Sidecans <110431851+Sidecans@users.noreply.github.com>
2022-08-12 18:16:02 -0700

    Update Cobra Shell

--- CobraVenom/cobra_shell.py
+++ CobraVenom/cobra_shell.py
@@ -1,19 +1,26 @@
 import time
 import os
-class Scanner():
-    def __init__(self, chunks):
-        self.__class__keyword = chunks[0]
-        try:    
-            self.command = chunks[1]
 
-        except:
-            print("Error: Errno [1]: Command Not Provided")
-
-        
-            
 with open("destination.txt", 'rb') as f:
     login = f.read()
+class Scanner():
+    def __init__(self, chunks):
+        self.keyword = chunks[0]
+        try:    
+            self.command = chunks[1]
+            self.exe_command()
+        except:
+            print("Error: Errno [1]: Command Not Provided")
+    def exe_command(self):
+        if self.keyword == "cd":
+            if os.path.exists(login + "\\" + self.command):
+                with open("destination.txt") as F:
+                    F.write(login + "\\" + self.command)
+            
+
 while True:
+    with open("destination.txt", 'rb') as f:
+        login = f.read()
     print(login)
     text = input("Cobra > ")
     chunks = text.split(' ')
