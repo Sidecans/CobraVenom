3f14889f67148ad7a97f169fe824451599e8a5c6
Sidecans <110431851+Sidecans@users.noreply.github.com>
2022-08-19 18:18:30 -0700

    Made Interpreter

--- CobraVenom/cobra_interpreter.py
+++ CobraVenom/cobra_interpreter.py
@@ -1,9 +1,11 @@
-def run(file):
-    with open(file, 'r') as f:
-        for count, line in enumerate(f):
-            pass
-        count += 1
-        print("Total Lines:" + str(count))
+class Scanner():
+    def __init__(self):
+        pass
+    def readline(self, lin):
+        chunks = lin.split(' ')
 
+
+            
+    
 
 
